ea14d2c462b836b7022cef81a9443470e24db2da
raulrp3 <raul.ramirez@escuelaestech.es>
2018-10-19 15:56:45 +0200

    Mejorando funciones

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,28 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    public static double validarRadio(Scanner sc){
-        double longitudValidado;
-        while (!sc.hasNextDouble()){
-            System.out.print("¡Introduce un valor numérico para el radio! ");
-            sc.next();
+    public static double validador(int opcion){
+        double validado = 0;
+        Scanner sc = new Scanner(System.in);
+        switch (opcion){
+            case  0:{
+                while(!sc.hasNextInt()){
+                    System.out.print("¡Por favor introduce un número entero! ");
+                    sc.next();
+                }
+                validado = sc.nextDouble();
+            }
+            break;
+            case 1:{
+                while(!sc.hasNextDouble()){
+                    System.out.print("¡Por favor introduce un número decimal! ");
+                    sc.next();
+                }
+                validado = sc.nextDouble();
+            }
+            break;
         }
-        longitudValidado = sc.nextDouble();
-        return longitudValidado;
+        return  validado;
     }
     public static double calculaLongitud(double radio){
         return 2 * Math.PI * radio;
@@ -20,7 +34,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("¡Hola!, vamos a calcular la longitud de una circunferencia.");
         System.out.print("Introduce radio: ");
-        radio = validarRadio(sc);
+        radio = validador(1);
         sc.close();
         longitud = calculaLongitud(radio);
         System.out.printf("El valor de la longitud de la circunferencia de radio %f es: %f\n", radio,longitud);
